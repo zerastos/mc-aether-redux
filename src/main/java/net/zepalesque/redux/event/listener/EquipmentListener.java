4cbc9df6f8fec722037ebb237a324c080d0ddf14
Zerastos <30709206+zerastos@users.noreply.github.com>
2026-08-11 22:36:57 +0200

    Sentry Ring only server side

--- src/main/java/net/zepalesque/redux/event/listener/EquipmentListener.java
+++ src/main/java/net/zepalesque/redux/event/listener/EquipmentListener.java
@@ -107,19 +107,17 @@ public class EquipmentListener {
                 }
         }
 
-        if (target != null && event.getSource().getDirectEntity() instanceof Player player && !event.getSource().is(ReduxDamageTypes.EMBER) && event.getAmount() > 1.0) {
+        if (target != null && !target.level().isClientSide() && event.getSource().getDirectEntity() instanceof Player player && !event.getSource().is(ReduxDamageTypes.EMBER) && event.getAmount() > 1.0) {
             if (EquipmentUtil.hasCurio(player, ReduxItems.SENTRY_RING.get())) {
                 int ringcount = EquipmentUtil.getCurios(player, ReduxItems.SENTRY_RING.get()).size();
                 RandomSource source = target.level().getRandom();
-                int embers = event.getAmount() < 1.5 || player.getMainHandItem().isEmpty() ? 0 : Mth.ceil(((event.getAmount() * 0.5D) + 1D) * (ringcount == 2 ? 1.5D : 1D)) - source.nextInt(1);
+                int embers = event.getAmount() < 1.5 || player.getMainHandItem().isEmpty() ? 0 : Mth.ceil(((event.getAmount() * 0.5D) + 1D) * (ringcount == 2 ? 1.5D : 1D));
                 for (int i = 1; i <= embers; i++) {
                     float rotation = Mth.wrapDegrees(source.nextInt(360));
                     Ember ember = new Ember(target.level(), player, target);
                     ember.setPos(target.getX(), target.getY() + (target.getBbHeight() / 2) + ((source.nextFloat() * 2) - 1), target.getZ());
                     ember.shootFromRotation(target, -45 + (90 * source.nextFloat()), rotation, 0.0F, 0.5F, 1.0F);
-                    if (!player.level().isClientSide()) {
-                        player.level().addFreshEntity(ember);
-                    }
+                    player.level().addFreshEntity(ember);
                 }
 
             }
